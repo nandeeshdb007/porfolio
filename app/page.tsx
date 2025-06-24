c9fc3aa521d3927d4a0a1a90c80680ed368ad41f
nandeesh_007 <nandeesh.db@bulkbeings.co>
2025-06-24 18:06:39 +0530

    hot-fix

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ const Home = () => {
     <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        {/* <Hero />
-        <Grid />
+        {/* <Hero /> */}
+        {/* <Grid /> */}
         {/* <RecentProjects /> */}
-        <Experience />
+        {/* <Experience /> */}
         {/* <Approach /> */}
-        <Footer /> */}
+        {/* <Footer /> */}
       </div>
     </main>
   );
